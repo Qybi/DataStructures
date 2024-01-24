a4dc78acf10c9eb39f29985b14f2a0a33c815635
Raffaele <raffaele.trivillin@stud.itsaltoadriatico.it>
2024-01-24 16:22:30 +0100

    Added Stack implementation

---
+++ DataStructures/DataStructures/Stack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.DataStructures
+{
+    internal class Stack
+    {
+        private int[] elements;
+        private int top;
+        private int size;
+        public Stack(int size)
+        {
+            elements = new int[size];
+            top = -1;
+            this.size = size;
+        }
+
+        public void push(int value)
+        {
+            if (isFull()) return;
+            else elements[++top] = value;
+        }
+        public int pop()
+        {
+            if (isEmpty()) return -1;
+            else
+            {
+                return elements[top--];
+            }
+        }
+        public int peek()
+        {
+            if (isEmpty()) return -1;
+            else return elements[top];
+        }
+        public bool isEmpty()
+        {
+            if (top == -1) return true;
+            else return false;
+        }
+        public bool isFull()
+        {
+            if (top == size - 1) return true;
+            else return false;
+        }
+        public void printStack()
+        {
+            if (size == 0) return;
+            
+            for (int i = 0; i <= top; i++)
+            {
+                Console.WriteLine($"position: {i} | value: {elements[i]}");    
+            }
+        }
+    }
+}
